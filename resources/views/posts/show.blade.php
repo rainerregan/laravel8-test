91cdb93fc664bc95b842d4b3b27014a7b21bb617
Rainer Regan <rainercoc@gmail.com>
2022-07-28 22:43:12 +0700

    Styling post page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,25 +3,11 @@
 @section('title', $post['title'])
 
 @section('content')
+    <h1>{{ $post->title }}</h1>
+    <p>{{ $post->content }}</p>
+    <p>Added {{$post->created_at->diffForHumans()}}</p>
 
-    @if ($post['is_new'])
-        <div>
-            <p>A new blog post!</p>
-        </div>
-    @elseif (!$post['is_new'])
-        <div>
-            <p>An old blog post!</p>
-        </div>
+    @if (now()->diffInMinutes($post->created_at) < 5)
+        <div class="alert alert-info">New!</div>
     @endif
-
-    @unless ($post['is_new'])
-        <div>It is an old post using unless</div>
-    @endunless
-
-    <h1>{{ $post['title'] }}</h1>
-    <p>{{ $post['content'] }}</p>
-
-    @isset($post['has_comments'])
-        <div>The post has some comments using isset.</div>
-    @endisset
 @endsection
