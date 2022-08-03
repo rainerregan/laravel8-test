af69174dd7b4cc5a9748284b746212d817d82895
Rainer Regan <rainercoc@gmail.com>
2022-08-03 20:42:53 +0700

    Displaying image thumbnail

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,20 +6,30 @@
     <div class="row">
         <div class="col-8">
 
-            <h1>
-                {{ $post->title }}
+            {{-- Jika memiliki image --}}
+            @if ($post->image)
+                <div style="background-image: url({{$post->image->url()}}); min-height: 500px; color:white; text-align:center; background-attachment:fixed;">
+                    <h1 style="padding-top:100px; text-shadow: 1px 2px #000;">
+            @else
+                    <h1>
+            @endif
+                        {{ $post->title }}
 
-                {{-- Nama components sudah di buat alias di AppServiceProvider, sehingga kita tidak perlu untuk memberikan parameter nama --}}
-                {{-- Update Laravel 8: Kita tidak perlu untuk membuat alias, tapi dengan menggunakan x-namaComponent sudah bisa --}}
-                <x-badge type="info" :show="now()->diffInMinutes($post->created_at) < 30">
-                    Brand new post!
-                </x-badge>
-            </h1>
+                        {{-- Nama components sudah di buat alias di AppServiceProvider, sehingga kita tidak perlu untuk memberikan parameter nama --}}
+                        {{-- Update Laravel 8: Kita tidak perlu untuk membuat alias, tapi dengan menggunakan x-namaComponent sudah bisa --}}
+                        <x-badge type="info" :show="now()->diffInMinutes($post->created_at) < 30">
+                            Brand new post!
+                        </x-badge>
+            @if ($post->image)
+                    </h1>
+                </div>
+            @else
+                    </h1>
+            @endif
 
 
             <p>{{ $post->content }}</p>
 
-            <img src="{{ $post->image->url() }}" alt="" srcset="">
 
             {{-- Created Timestamp Component --}}
             <x-updated :date="$post->created_at" :name="$post->user->name"></x-updated>
