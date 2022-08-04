999429625c25b89ea0e3ce0b59a1737d1dbb460f
Rainer Regan <rainercoc@gmail.com>
2022-08-04 21:15:33 +0700

    Email template and sending email

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreComment;
+use App\Mail\CommentPosted;
 use App\Models\BlogPost;
+use Illuminate\Support\Facades\Mail;
 
 /**
  * Post Comment Controller
@@ -19,10 +21,15 @@ class PostCommentController extends Controller
 
     public function store(BlogPost $post, StoreComment $request)
     {
-        $post->comments()->create([
+        $comment = $post->comments()->create([
             'content' => $request->input('content'),
             'user_id' => $request->user()->id
         ]);
+
+        // Sending email
+        Mail::to($post->user)->send(
+            new CommentPosted($comment)
+        );
 
         return redirect()->back()->withStatus('Comment was created!');
     }
