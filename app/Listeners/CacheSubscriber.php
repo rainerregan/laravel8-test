cc6f71ba112ab10a030c59a0615730bbfb37ab5a
Rainer Regan <rainercoc@gmail.com>
2022-08-06 08:54:31 +0700

    Using subscriber for logging

---
+++ app/Listeners/CacheSubscriber.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Cache\Events\CacheHit;
+use Illuminate\Cache\Events\CacheMissed;
+use Illuminate\Support\Facades\Log;
+
+class CacheSubscriber
+{
+    public function handleCacheHit(CacheHit $event)
+    {
+        Log::info("{$event->key} cache hit");
+    }
+
+    public function handleCacheMissed(CacheMissed $event)
+    {
+        Log::info("{$event->key} cache miss");
+    }
+
+    public function subscribe($events)
+    {
+        $events->listen(
+            CacheHit::class,
+            [CacheSubscriber::class, 'handleCacheHit']
+        );
+
+        $events->listen(
+            CacheMissed::class,
+            [CacheSubscriber::class, 'handleCacheMissed']
+        );
+    }
+}
